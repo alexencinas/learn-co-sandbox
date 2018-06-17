767d60a1a67dd1593061a06324e27ad07891f3c3
alexencinas <shoogiee123@gmail.com>
2018-06-17 07:23:20 +0000

    automatically backed up by learn

---
+++ fortune_teller./model.rb
@@ -0,0 +1,6 @@
+def get_fortune
+  fortunes_array = ["You will marry Shawn Mendes", "You will win 20 goats soon", "You will win $20 but have to spend it on a cheap sweater", "You will win a Nobel Prize for computer science research"]
+  return fortunes_array.sample
+end 
+
+  
